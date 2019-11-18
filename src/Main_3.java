71ec22eddbe7af7c4619f26c57b6fd8291a96e20
belostotskaya18 <57672984+belostotskaya18@users.noreply.github.com>
2019-11-18 15:17:55 +0300

    Create Main_3.java

---
+++ src/Main_3.java
@@ -0,0 +1,13 @@
+public class Main {
+        public static void main(String[] args) {
+            Person p = new Person("Test", 3);
+            System.out.println(p);
+            p.setAge(5);
+            p.setName("Vasya");
+            System.out.println(p);
+            System.out.println(p.work());
+            Employee e = new Employee("1", 2, "3");
+            System.out.println(e);
+            System.out.println(e.work());
+        }
+}
