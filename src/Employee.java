eb4433c4ebb69a3280004e992591cdcb0086601a
belostotskaya18 <57672984+belostotskaya18@users.noreply.github.com>
2019-11-18 15:07:51 +0300

    Create Employee.java

---
+++ src/Employee.java
@@ -0,0 +1,14 @@
+public class Employee extends Person {
+    private String company;
+
+    Employee(String name, int age, String company) {
+        super(name, age);
+        this.company = company;
+    }
+    
+    @Override
+    public String toString() {
+        String a = Integer.toString(age);
+        return new StringBuilder().append("(").append(name).append(", ").append(age).append(", ").append(company).append(")").toString();
+    }
+}
