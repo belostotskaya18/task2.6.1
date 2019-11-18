45e2acc29f03fc304a979ae4dd1f0a9bdab9ab98
belostotskaya18 <57672984+belostotskaya18@users.noreply.github.com>
2019-11-18 15:07:11 +0300

    Create Person_2.java

---
+++ src/Person_2.java
@@ -0,0 +1,31 @@
+public class Person {
+    public int age;
+    public String name;
+
+    Person(String name, int age) {
+        this.age = age;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    @Override
+    public String toString() {
+        String a = Integer.toString(age);
+        return new StringBuilder().append("(").append(name).append(", ").append(age).append(")").toString();
+    }
+
+}
